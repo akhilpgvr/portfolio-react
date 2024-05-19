c067270013f1e7ec7b676037159a73c36eb5af2e
Akhil <akhilpgvr@gmail.com>
2024-05-20 01:26:15 +0530

    completed footr and started doing navigation menu traversal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import underline from '../../assets/nav_underline.svg'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
   return (
     <div className='navbar'>
       <img src={logo} alt="Space for logo" />
       <ul className='nav-menu'>
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Contact</li>
-        <li>Contact</li>
+        <li><p onClick={()=> setMenu("home")}>Home</p>{menu==="home"?<img src={underline} /> : <></>}</li>
+        <li><p onClick={()=> setMenu("about")}>About Me</p>{menu==="about"?<img src={underline} /> : <></>}</li>
+        <li><p onClick={()=> setMenu("services")}>Services</p>{menu==="services"?<img src={underline} /> : <></>}</li>
+        <li><p onClick={()=> setMenu("work")}>Portfolio</p>{menu==="work"?<img src={underline} /> : <></>}</li>
+        <li><p onClick={()=> setMenu("contact")}>Contact</p>{menu==="contact"?<img src={underline} /> : <></>}</li>
       </ul>
       <div className="nav-connect">Connect with me</div>
     </div>
