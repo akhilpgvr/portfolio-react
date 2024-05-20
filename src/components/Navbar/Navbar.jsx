e16f6dc665b5db0466252b4cc6c11d51ffd5281e
Akhil <akhilpgvr@gmail.com>
2024-05-21 00:41:58 +0530

    making responsive for mob -- converted nav menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () =>{
+    menuRef.current.style.right="0"
+  }
+  const closeMenu = () =>{
+    menuRef.current.style.right="-350px"
+  }
+
   return (
     <div className='navbar'>
       <img src={logo} alt="Space for logo" />
-      <ul className='nav-menu'>
+      <img src={menu_open} onClick={openMenu} alt="Space for menu_open" className='nav-mob-open'/>
+      <ul ref ={menuRef} className='nav-menu'>
+        <img src={menu_close} onClick={closeMenu} alt="Space for menu_close" className="nav-mob-close" />
         <li> <AnchorLink className='anchor-link' href='#home'> <p onClick={()=> setMenu('home')}>Home</p> </AnchorLink> {menu==='home'? <img src={underline} /> : <></>}</li>
         <li> <AnchorLink className='anchor-link' offset={50} href='#about'> <p onClick={()=> setMenu('about')}>About Me</p>  </AnchorLink> {menu==='about'? <img src={underline} /> : <></>}</li>
         <li> <AnchorLink className='anchor-link' offset={50} href='#services'> <p onClick={()=> setMenu('services')}>Services</p> </AnchorLink> {menu==='services'? <img src={underline} /> : <></>}</li>
